b0590ede16c98c032e4e81dc433153f0305f3bea
dongdapeng2012 <dongdapeng2012@sina.com>
2020-04-15 00:19:38 +0800

    remove unused import

--- src/main/java/com/geth/autotestjava/SeleniumTest.java
+++ src/main/java/com/geth/autotestjava/SeleniumTest.java
@@ -2,8 +2,6 @@ package com.geth.autotestjava;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
